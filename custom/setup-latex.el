6fc2205bffb8a6a2874f4b39a2d2383dcb941f56
tmadlener <thomas.madlener@cern.ch>
2017-01-12 12:14:39 +0100

    removed use-package usage from latex setup as it was not working

--- custom/setup-latex.el
+++ custom/setup-latex.el
@@ -1,42 +1,40 @@
 ;; setup everything concerning latex
 
-(use-package tex
-  :ensure auctex
-  :mode ("\\.tex\\'" . TeX-latex-mode)
-  :init
-  (load "auctex.el" nil t t)
+(load "auctex.el" nil t t)
 
-  (setq-default TeX-PDF-mode t)
-  
-  (defun auctex-hook-function()
-    (setq TeX-auto-save t)
-    (setq TeX-parse-self t) ;; parse document
-    
-    (setq-default TeX-master nil) ;; make AUCTex ask for a master (main file in which other files are included)
-    (TeX-source-correlate-mode t) ;; synchronize evince and source
-    )
+(setq-default TeX-PDF-mode t)
 
-  (add-hook 'LateX-mode-hook 'auctex-hook-function)
-  (add-hook 'LateX-mode-hook 'LaTeX-math-mode)
-  (add-hook 'Latex-mode-hook 'turn-on-reftex)
+(defun auctex-hook-function()
+  (setq TeX-auto-save t)
+  (setq TeX-parse-self t) ;; parse document
 
-  (require 'latex)
-  (TeX-global-PDF-mode t)
-  (setq TeX-save-query nil) ;; auto save on tex-command
-
-  (setq reftex-plug-into-AUCTEX t)
-
-  ;; highlight some user definend commands.
-  ;; using the auctex warning feature for this (NOTE: only keywords, not arguments get highlighted)
-  ;; a more sophisticated approach would be to define a face-lock for this
-  (setq font-latex-match-warning-keywords
-	'(
-	  ("citeme" "{")
-	  ("comment" "{")
-	  ("todo" "{")
-	  ("refmis" "{")
-	  )
-	)
+  (setq-default TeX-engine 'xetex) ;; use xetex by default
+  (setq-default TeX-master nil) ;; make AUCTex ask for a master (main file in which other files are included)
+  (TeX-source-correlate-mode t) ;; synchronize evince and source
+  (message "auctec-hook-function called")
   )
 
+(add-hook 'LateX-mode-hook 'auctex-hook-function)
+(add-hook 'LateX-mode-hook 'LaTeX-math-mode)
+(add-hook 'Latex-mode-hook 'turn-on-reftex)
+
+(require 'tex)
+(TeX-global-PDF-mode t)
+(setq TeX-save-query nil) ;; auto save on tex-command
+
+(setq reftex-plug-into-AUCTEX t)
+
+;; highlight some user definend commands.
+;; using the auctex warning feature for this (NOTE: only keywords, not arguments get highlighted)
+;; a more sophisticated approach would be to define a face-lock for this
+(setq font-latex-match-warning-keywords
+      '(
+        ("citeme" "{")
+        ("comment" "{")
+        ("todo" "{")
+        ("refmis" "{")
+        )
+      )
+
 (provide 'setup-latex)
+;;; setup-latex.el ends here
